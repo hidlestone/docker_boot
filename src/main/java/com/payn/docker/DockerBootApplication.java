411bdc79448e4f11ee8a86c4ed2bc590f1194b92
zhuangpf <809566095@qq.com>
2025-02-20 12:35:29 +0800

    初始

---
+++ src/main/java/com/payn/docker/DockerBootApplication.java
@@ -0,0 +1,17 @@
+package com.payn.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuangpf
+ * @date 2023-01-03
+ */
+@SpringBootApplication
+public class DockerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBootApplication.class, args);
+	}
+
+}
